37323653e761c24271993322c1ea40e050dba59b
Gushihiro <robinsonhiroto@gmail.com>
2021-07-24 13:40:50 -0700

    getting deploy working

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path='/venueartscenter'>
+        <Route exact path=''>
           <Main/>
         </Route>
       </Router>
